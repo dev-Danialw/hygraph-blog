6c22ca5c8056246517678ba388659e99b655e9ef
Danial-Waheed <daniyalwahied@gmail.com>
2022-09-19 23:04:00 +0500

    Comments functionality

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ posts }) {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
+          {posts.map((post) => (
             <PostCard post={post.node} key={post.title} />
           ))}
         </div>
